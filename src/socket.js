878930941f85ef3472ae47aeac2c90199e5eb13a
Nguyễn Trọng Hoàng <tronghoang1907@gmail.com>
2023-12-15 18:35:11 +0700

    1

--- src/socket.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// socket.ts
-io.on("connection", (socket) => {
-    console.log("A user connected", socket.id);
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-});
